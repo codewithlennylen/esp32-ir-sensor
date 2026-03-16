45ddbfbd0e11d8e0bee61dcbc528afb263e62725
Lenny Ng'ang'a <codewithlennylen254@gmai.com>
2026-03-16 21:10:51 +0300

    Analog and Digital Read Outs

--- esp32_ir/src/main.cpp
+++ esp32_ir/src/main.cpp
@@ -1,19 +1,41 @@
 #include <Arduino.h>
 
 
-const int ledPin = 2; // Internal LED is usually on GPIO 2
+const int d0Pin = 14; // Digital Output pin
+const int a0Pin = 12; // Analog Output pin
+
+byte d0Read;
+uint16_t a0Read;
 
 void setup() {
 
+  Serial.begin(9600);
+  pinMode(d0Pin, INPUT);
+  pinMode(2, OUTPUT);
 
-  pinMode(ledPin, OUTPUT);
+  // Blink internal LED to show initialization status
+  digitalWrite(2, HIGH);
+  delay(300);
+  digitalWrite(2, LOW);
+  delay(300);
+  digitalWrite(2, HIGH);
+  delay(300);
+  digitalWrite(2, LOW);
+  delay(300);
+
+  Serial.println("ESP32 Initialized");
 
 }
 
 void loop() {
 
-  digitalWrite(ledPin, HIGH); // Turn LED ON
-  delay(1000);
-  digitalWrite(ledPin, LOW);  // Turn LED OFF
-  delay(1000);
+  d0Read = digitalRead(d0Pin);
+  a0Read = analogRead(a0Pin);
+
+  Serial.print("D0: ");
+  Serial.print(d0Read);
+  Serial.print(" A0: ");
+  Serial.println(a0Read);
+
+  delay(500);
 }
